66612222a873012af1abc95a9b9c8ccc87a5b84c
YBLim13 <77183367+YBLim13@users.noreply.github.com>
2022-05-02 10:50:12 -0400

    Add Logos

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,14 +1,12 @@
 const Images = {
-  
-
-
   productPlaceholder: require("./productPlaceholder.png"),
   search: require("./search.png"),
   gtLogo: require("./gtLogo.png"),
   placeholderImage: require("./placeholderImage.png"),
   camera: require("./camera.png"),
   universityIcon: require("./universityIcon.jpg"),
-  emoryLogo: require("./emoryLogo.png")
+  gsuLogo: require("./gsuLogo.jpg"),
+  ugaLogo: require("./ugaLogo.png")
 };
 
 export default Images;
